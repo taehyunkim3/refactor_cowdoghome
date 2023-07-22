9f416cf4493409f8ebe7f15f58ad8b3fb5686bef
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-22 14:21:20 +0900

    feat: input 완료

--- src/components/shared/InputContainer/InputContainer.jsx
+++ src/components/shared/InputContainer/InputContainer.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import { Input, InputBox } from "./style";
 
-export const InputContainer = ({}) => {
+export const InputContainer = ({ error, width, height, type, placeholder }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const emWidth = width ? `${parseInt(width) / 16}em` : "18.75em";
+  const emHeight = height ? `${parseInt(height) / 16}em` : "3.125em";
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
-    <InputBox>
-      <Input />
+    <InputBox
+      error={error}
+      width={emWidth}
+      height={emHeight}
+      isFocused={isFocused}
+    >
+      <label>
+        <Input
+          type={type}
+          placeholder={placeholder}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+      </label>
     </InputBox>
   );
 };
